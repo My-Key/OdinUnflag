b96fca96833d67f76ccdc22b3fcaf324d0bec847
Maciej <0my0key0@gmail.com>
2025-12-28 20:07:35 +0100

    Fix errors

--- UnflagEnum/Editor/UnflagEnumAttributeDrawer.cs
+++ UnflagEnum/Editor/UnflagEnumAttributeDrawer.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
-using Sirenix.Utilities;
 using Sirenix.Utilities.Editor;
 using UnityEditor;
 using UnityEngine;
@@ -14,6 +13,7 @@ public class UnflagEnumAttributeDrawer<T> : OdinAttributeDrawer<UnflagEnumAttrib
 {
 	private GUIContent m_buttonContent = new GUIContent();
 	private SdfIconType m_sdfIcon;
+	private string m_tooltip;
 	
 	public override bool CanDrawTypeFilter(Type type)
 	{
@@ -25,8 +25,9 @@ public class UnflagEnumAttributeDrawer<T> : OdinAttributeDrawer<UnflagEnumAttrib
 	private void UpdateButtonContent()
 	{
 		m_buttonContent.text = !EditorGUI.showMixedValue
-			? UnflagEnumSelector<T>.GetValueStringReflection(ValueEntry.SmartValue, out m_sdfIcon)
+			? UnflagEnumSelector<T>.GetValueStringReflection(ValueEntry.SmartValue, out m_sdfIcon, out m_tooltip)
 			: "-";
+		m_buttonContent.tooltip = m_tooltip;
 	}
 
 	protected override void DrawPropertyLayout(GUIContent label)
@@ -38,36 +39,20 @@ public class UnflagEnumAttributeDrawer<T> : OdinAttributeDrawer<UnflagEnumAttrib
 		else
 			rect = EditorGUI.PrefixLabel(rect, label);
 
-		var clicked = EditorGUI.DropdownButton(rect, new GUIContent(), FocusType.Keyboard);
-		
-		if (Event.current.type == EventType.Repaint)
-		{
-			if (m_sdfIcon != SdfIconType.None)
-				SdfIcons.DrawIcon(rect.TakeFromLeft(rect.height * 1.2f).Padding(2f, 0.0f, 3f, 3f), m_sdfIcon);
-		}
+		var output = UnflagEnumSelector<T>.DrawSelectorDropdown(rect, m_buttonContent, 
+			buttonRect =>
+			{
+				var selector =  new UnflagEnumSelector<T>();
+				selector.ShowInPopup(buttonRect.position + Vector2.up * buttonRect.height);
+				selector.SetSelection(ValueEntry.SmartValue);
+				return selector;
+			}, true, null, m_sdfIcon);
 
-		var labelRect = rect;
-		labelRect.width -= 16;
-		labelRect.x += 2;
-		GUI.Label(labelRect,m_buttonContent);
-		
-		if (!clicked)
+		if (output == null || !output.Any())
 			return;
 
-		var selector = new UnflagEnumSelector<T>();
-		
-		selector.SetSelection(ValueEntry.SmartValue);
-		selector.ShowInPopup(rect.position + Vector2.up * rect.height);
-
-		selector.SelectionChanged += x =>
-		{
-			ValueEntry.Property.Tree.DelayAction(() =>
-			{
-				ValueEntry.SmartValue = x.FirstOrDefault();
-
-				UpdateButtonContent();
-			});
-		};
+		ValueEntry.SmartValue = output.First();
+		UpdateButtonContent();
 	}
 
 	public class UnflagEnumSelector<TEnum> : EnumSelector<TEnum> where TEnum : Enum
@@ -81,7 +66,7 @@ public class UnflagEnumAttributeDrawer<T> : OdinAttributeDrawer<UnflagEnumAttrib
 			base.BuildSelectionTree(tree);
 
 			if (DrawOnlySingleOptions)
-				tree.MenuItems.RemoveAll(x => !IsSingleBit(((EnumTypeUtilities<TEnum>.EnumMember)x.Value).Value));
+				tree.MenuItems.RemoveAll(x => x.Value != null && !IsSingleBit(((EnumTypeUtilities<TEnum>.EnumMember)x.Value).Value));
 			else
 				tree.MenuItems.RemoveAll(x =>
 					((EnumTypeUtilities<TEnum>.EnumMember)x.Value).Value.Equals(GetZeroValue()));
@@ -120,7 +105,7 @@ public class UnflagEnumAttributeDrawer<T> : OdinAttributeDrawer<UnflagEnumAttrib
 		
 		private static MethodInfo m_getValueString;
 		
-		public static string GetValueStringReflection(TEnum valueEntrySmartValue, out SdfIconType sdfIcon)
+		public static string GetValueStringReflection(TEnum valueEntrySmartValue, out SdfIconType sdfIcon, out string tooltip)
 		{
 			if (m_getValueString == null)
 				m_getValueString =
@@ -128,9 +113,11 @@ public class UnflagEnumAttributeDrawer<T> : OdinAttributeDrawer<UnflagEnumAttrib
 						BindingFlags.Static | BindingFlags.NonPublic);
 
 			sdfIcon = SdfIconType.None;
-			var args = new[] { (object)valueEntrySmartValue, sdfIcon };
+			tooltip = null;
+			var args = new[] { (object)valueEntrySmartValue, sdfIcon, tooltip };
 			var result = (string)m_getValueString.Invoke(null, args);
 			sdfIcon = (SdfIconType)args[1];
+			tooltip = (string)args[2];
 			return result;
 		}
 		
