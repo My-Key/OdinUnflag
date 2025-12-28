657ef3c36dca3a5656f04d70d38275ca0347d174
Maciej <0my0key0@gmail.com>
2025-12-28 19:54:20 +0100

    Update for Visucal Designer

--- UnflagEnum/UnflagEnumAttribute.cs
+++ UnflagEnum/UnflagEnumAttribute.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+	
+#if UNITY_EDITOR
+using Sirenix.OdinInspector.Editor;
+
+[assembly: OdinVisualDesignerAttributeItem("VFX", typeof(VFXPropertySelectorAttribute))]
+#endif
 
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-public class UnflagEnumAttribute : Attribute
-{
-}
+public class UnflagEnumAttribute : Attribute { }
